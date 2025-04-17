b808cd3e20b6027978e271b1d3d9a3cf6a835669
Snehal Kumar <snehalkumar18@gmail.com>
2025-04-16 19:40:08 -0700

    Added navbar to contact page

--- contact.php
+++ contact.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="./styles/nav.css">
     <link type="text/css" rel="stylesheet" href="./styles/auth.css">
     <link type="text/css" rel="stylesheet" href="./styles/contact.css">
 </head>
 <body>
+    <?php include 'nav.php'; ?>
+
     <div class="container">
-        <h1 class="title" style="width: 16vw;">Contact Us</h1>
+        <h1 id="contact-us" class="title" style="width: 16vw; margin-top: 80px;">Contact Us</h1>
 
         <div class="content-container contact">
+            <!-- Left Side -->
             <form id="contact-form" class="left-column" action="contact.php" method="GET" autocomplete="off">
                 <div style="margin-top: 40px;">
                     <input id="uname" type="text" name="uname" placeholder="Username" required><br>
@@ -28,6 +34,7 @@
                 <input id="contact-submit" class="submit" type="submit" name="contactSubmit" value="Send Message">
             </form>
 
+            <!-- Right Side -->
             <div class="right-column">
                 <p style="font-weight: bold; font-size: 1.2vw;">Have a question, concern, or feedback?</p>
                 <p>Fill out this form!</p>
