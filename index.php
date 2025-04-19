ca8fedd3a62314723fafdb388e12544f018ce7a0
Snehal Kumar <snehalkumar18@gmail.com>
2025-04-18 23:20:41 -0700

    Api call to postLike file to insert or delete user's like on a post

--- index.php
+++ index.php
@@ -27,27 +27,32 @@
         $query->execute();
 
         $result = $query->get_result();
+        if ($result->num_rows > 0) $rows = $result->fetch_all(MYSQLI_ASSOC);
 
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $likes = $row['likeCount'] ?? 0;
-                $color = $categoryColors[$row['name']] ?? 'gray';
+        // Build posts html
+        forEach($rows as $row) {
+            $likes = $row['likeCount'] ?? 0;
+            $color = $categoryColors[$row['name']] ?? 'gray';
 
-                // Build post div
-                $postDiv .= "<div id='{$row['pid']}' class='post'>
-                                <div>
-                                    <div class='postInfo'>
-                                        <div>
-                                            <span><strong style='color: {$color};'>{$row['name']}</strong> &bull; pikachu &bull; <span style='color: lightgray;'>{$row['created_at']}</span></span>
-                                        </div>
-                                        <span style='color: salmon;'>{$likes}</span>
+            // Check if user has liked the post
+            isset($_SESSION['postIDS']) && in_array($row['pid'], $_SESSION['postIDS']) ? $thumbsUpClass = 'thumbs-up-success' : $thumbsUpClass = 'thumbs-up';
+
+            $postDiv .= "<div id='{$row['pid']}' class='post'>
+                            <div>
+                                <div class='post-info'>
+                                    <div>
+                                        <span><strong style='color: {$color};'>{$row['name']}</strong> &bull; pikachu &bull; <span style='color: lightgray;'>{$row['created_at']}</span></span>
                                     </div>
-                                    <span class='postTitle'>{$row['title']}</span>
+                                    <div>
+                                        <i class='fa-solid fa-thumbs-up {$thumbsUpClass}'></i>
+                                        <span class='like-count'>{$likes}</span>
+                                    </div>
                                 </div>
-                            </div>";
-            }
+                                <span class='post-title'>{$row['title']}</span>
+                            </div>
+                        </div>";
         }
-
+        
         // Close connection to database 
         closeConnection($db);
     }
@@ -66,6 +71,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dialogue</title>
+    <script src="https://kit.fontawesome.com/329329b608.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./styles/nav.css">
@@ -85,11 +91,61 @@
         <!-- Display posts -->
         <?= $postDiv ?>
 
+        <script>
+            // Fetch all elements (posts) with class 'post'
+            const posts = document.querySelectorAll('.post');
+
+            posts.forEach(post => {
+                const postID = post.id;
+                let thumbsUp = post.querySelector('i');
+                let likeCount = post.querySelector('.like-count');
+
+                // Add event listeners to all posts
+                post.addEventListener('click', () => {
+                    // console.log(post);
+                });
+
+                // Add event listener to thumbs-up icon
+                thumbsUp.addEventListener('click', async () => {
+                    // Source: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest_API/Using_FormData_Objects
+                    const formData = new FormData();
+                    formData.append('postID', postID);
+
+                    const res = await fetch('postLike.php', {
+                        method: 'POST',
+                        body: formData
+                    });
+
+                    const result = await res.json();
+                    if (result.success) {
+                        // Post is liked 
+                        if (result.status) {
+                            thumbsUp.classList.remove('thumbs-up');
+                            thumbsUp.classList.add('thumbs-up-success');
+                            likeCount.innerHTML = Number(likeCount.innerHTML) + 1;
+                        }
+                        // Post is unliked
+                        else {
+                            thumbsUp.classList.remove('thumbs-up-success');
+                            thumbsUp.classList.add('thumbs-up');
+                            likeCount.innerHTML = Number(likeCount.innerHTML) - 1;
+                        }
+                    } 
+                    else alert(result.message);
+                });
+            });
+        </script>
+
         <!-- <div class="post">
             <div>
                 <div class="postInfo">
-                    <div><span><strong style="color: orange;">Technology</strong> &bull; pikachu &bull; <span style="color: lightgray;">2025-04-17</span></span></div>
-                    <span style="color: salmon;">10</span>
+                    <div>
+                        <span><strong style="color: orange;">Technology</strong> &bull; pikachu &bull; <span style="color: lightgray;">2025-04-17</span></span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-thumbs-up thumbs-up"></i>
+                        <span style="color: salmon;">10</span>
+                    </div>
                 </div>
                 <span class="postTitle">Is it worth getting TSA PreCheck?</span>
             </div>
@@ -106,5 +162,4 @@
         </div> -->
     </div>
 </body>
-    <script src="script.js"></script>
 </html>
---
+++ postLike.php
@@ -0,0 +1,42 @@
+<?php
+    require_once 'config.php';
+
+    if (isset($_SESSION['username']) && isset($_POST['postID'])) {
+        try {
+            // Create connection to database
+            $db = getConnection();
+
+            // Add or remove like from database
+            $query = $db->prepare("CALL UpdatePostLikes(?, ?)");
+            $query->bind_param('si', $_SESSION['username'], $_POST['postID']);
+            $query->execute();
+
+            $result = $query->get_result();
+            $row = $result->fetch_assoc();
+
+            if ($row['Message'] === 'liked') {
+                $status = true;
+                // Add postID of liked post to session
+                $_SESSION['postIDS'][] = $_POST['postID'];
+            }
+            else {
+                $status = false;
+                // Remove postID of unliked post from session
+                $_SESSION['postIDS'] = array_diff($_SESSION['postIDS'], [$_POST['postID']]);
+            }
+
+            // Close connection to database
+            closeConnection($db);
+
+            // Send success response
+            echo json_encode(['success' => true, 'status' => $status]);
+        }
+        catch (Exception $e) {
+            error_log("Like post error: {$e->getMessage()}\n");
+        }
+    }
+    else {
+        // Send error response
+        echo json_encode(['success' => false, 'message' => 'You must be logged in to like posts']);
+    }
+?>
